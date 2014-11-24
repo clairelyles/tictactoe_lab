7b90a5ae2eac246b37e34d388199f9227c38dce0
clairelyles <claire.lyles@gmail.com>
2014-11-23 22:41:28 -0800

    Added a refresh button and winning combo detector

--- tictactoe.js
+++ tictactoe.js
@@ -1,26 +1,60 @@
 
 var counter = 0;
+var clearBoard = "";
 
 
 document.addEventListener('DOMContentLoaded', function(){
 	var square = document.getElementsByClassName('cell');
-	// console.log returns an arraty of elements with class name "cell"
-	// e.g. [div.col-]
-	//console.log(box);
+	var refresh = document.getElementById('refresh');
+	var winningCombo = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]];
 
-	//addeventlistener is a function that will look through each square 
-	// on the page which is a DOM object
+	function checkForWinner() {
+		// each click runs checkForWinner function
+		console.log('checking',square);
+		for (var i = 0; i < winningCombo.length; i++) {
+			if (square[winningCombo[i][0]].innerText !== "" &&
+				square[winningCombo[i][0]].innerText === square[winningCombo[i][1]].innerText &&
+				square[winningCombo[i][1]].innerText === square[winningCombo[i][2]].innerText) {
+				alert('You won ' + square[winningCombo[i][2]].innerText + ". Great job!");
+				return square[winningCombo[i][2]].innerText;
+			};
+		}
+	}
+
+	refresh.addEventListener('click', function() {
+		for (i = 0; i < square.length; i++) {
+			console.log('alert!');
+			square[i].innerText = clearBoard
+		}
+	});
+
+
+	// console.log(squares[i]innerText);
 	for (var i = 0; i < square.length; i++) {
 		square[i].addEventListener("click", function() {
-			this.innertext
+			// alert(this.innerText);
+			if (this.innerText === "X" || this.innerText === "O") {
+				return;
+			}
+			// else
 			counter += 1;
+
 			if (counter % 2 === 0) {
-				this.innerText = "0";
+				this.innerText = "O";
 			}
 			else {
 				this.innerText = "X";
 			}
+			checkForWinner();
 		})
+
 	}
+
+
+	// Winning Combinations
+	// winningCombo[i][0] && winningCombo[i][1] && winningCombo[i][3]
+
 });
+
+
 
